5d6af710475767032a14802c4481d9ff3c10233b
imzackadams <imzackadams@protonmail.com>
2025-03-19 15:11:57 -0400

    api connection passed

--- solana-payments-sdk/server.js
+++ solana-payments-sdk/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -6,9 +7,9 @@ const app = express();
 app.use(express.static('examples/simple-payment'));
 app.use('/dist', express.static('dist'));
 
-// API endpoint for Helius key (mock for testing)
+// API endpoint for Helius key
 app.get('/api/get-key', (req, res) => {
-  res.json({ apiKey: 'YOUR_HELIUS_API_KEY_HERE' });
+  res.json({ apiKey: process.env.HELIUS_KEY });
 });
 
 const PORT = 3000;
