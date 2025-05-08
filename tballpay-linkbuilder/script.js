61ec482c35f466f5fc80322c7b51d23e02fe761e
imzackadams <imzackadams@protonmail.com>
2025-05-08 16:09:07 -0400

    fix deployment url

--- tballpay-linkbuilder/script.js
+++ tballpay-linkbuilder/script.js
@@ -9,7 +9,8 @@ document.getElementById('builderForm').addEventListener('submit', (e) => {
 
 
   //testing local
-    const baseUrl = 'https://tballcheckout-pff9zgb4j-imzackadams-projects.vercel.app';
+  const baseUrl = 'https://tballcheckout-nwd9014tl-imzackadams-projects.vercel.app';
+
   
 
     const link = `${baseUrl}/?recipient=${recipient}&amount=${amount}&label=${label}`;
